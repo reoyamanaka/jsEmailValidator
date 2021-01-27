6ed40cf71bbf6a37c9821928c9b80e9247cf29f1
Reo Yamanaka <ryamanaka807@gmail.com>
2021-01-27 03:58:47 -0500

    finished validation using ajax and jquery

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 $(document).ready(() => {
     $(() => {
         $('#submit').on('click', (e) => {
-            e.preventDefault();
-            var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-            
+            var mailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+            var email = document.getElementById('email').value;
+            //check that email is not empty
+            if (email === ""){
+                alert('Email required.');
+                return false;
+            } else {
+                if(email.match(mailFormat)){
+                    alert("Valid email address.");
+                    return true;
+                } else {
+                    alert("You have entered an invalid email address.");
+                    email.focus();
+                    return false;
+                }
+            }
         });
     })
 });
